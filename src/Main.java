a278a4e7ec6dcd61148e92d9f03b42f03478e4c8
Andrey Iljasov <a_iljasov@mail.ru>
2022-08-29 22:32:29 +0300

    Rewrite with lambda

--- src/Main.java
+++ src/Main.java
@@ -3,35 +3,56 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
-import static java.util.Comparator.nullsFirst;
 
 public class Main {
     public static void main(String[] args) {
 
-        PersonComparator personComparator = new PersonComparator(4);
         List<Person> personList = new ArrayList<>();
+        Comparator<Person> personComparatorLambda = (o1, o2) -> {
+            if (o1 == null) {
+                return -1;
+            }
+            if (o2 == null) {
+                return 1;
+            }
+
+            int surname1 = o1.getSurname().split(" ").length;
+            int surname2 = o2.getSurname().split(" ").length;
+            int age1 = o1.getAge();
+            int age2 = o2.getAge();
+
+            if (surname1 >= 4 && surname2 >= 4) {
+                return Integer.compare(age1, age2);
+            }
+
+            if (surname1 > surname2) {
+                return 1;
+            } else if (surname1 < surname2) {
+                return -1;
+            } else {
+                return Integer.compare(age1, age2);
+            }
+        };
 
         personList.add(null);
         personList.add(new Person("Михаил", "Паниковский (человек без паспорта)", 60));
         personList.add(new Person("Остап", "Берта Мария Бендер Бей (Бендер-Задунайский)", 33));
         personList.add(new Person("Атос", "граф де Ля Фер", 35));
         personList.add(new Person("Генри", "Индиана Джонс младший", 45));
-        personList.add(null);
         personList.add(new Person("Оскар", "де Ля Хойя", 25));
         personList.add(new Person("Гай", "Юлий Цезарь", 55));
         personList.add(new Person("Владимир", "Красное Солнышко", 50));
-        personList.add(new Person("Вася", "Иванов", 15));
+        personList.add(new Person("Вася", "Иванов", 18));
         personList.add(new Person("Вася", "Пупкин", 18));
         personList.add(null);
 
-        Collections.sort(personList, personComparator);
+        personList.sort(personComparatorLambda);
         System.out.println("Список->>");
 
         for (Person person : personList) {
             System.out.println(person);
         }
 
-        System.out.println("Удален элемент: " + personList.remove(0));
         System.out.println("Удален элемент: " + personList.remove(personList.indexOf(null)));
         System.out.println("Удален элемент: " + personList.remove(personList.indexOf(null)));
 
